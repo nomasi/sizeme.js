634c4f7523614e9c5bbd62606b09f4805fe9ff39
Ville Könönen <ville@nomasi.com>
2016-02-05 17:13:20 +0200

    Added first WooCommerce specific files.  Gulpfile.coffee NOT updated yet.

---
+++ src/ui/js/woocommerce/sizeme-woocommerce.js
@@ -0,0 +1,11 @@
+/* globals jQuery: false */
+
+var sizeme_UI_options = {
+    appendContentTo: ".variations_form",
+    appendSplashTo: ".sizeme-selection-container",
+    sizeSelectionContainer: ".sizeme-selection-container",
+	addToCartElement: ".single_add_to_cart_button",
+	addToCartEvent: "click"
+};
+
+jQuery.noConflict(true);
