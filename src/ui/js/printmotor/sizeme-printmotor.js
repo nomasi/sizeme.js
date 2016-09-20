c02686054bf9faa48d66ca67b02f505e1aa19f6f
Ville Könönen <ville@nomasi.com>
2016-09-20 19:36:04 +0300

    Fix candidate #2 for Printmotor

--- src/ui/js/printmotor/sizeme-printmotor.js
+++ src/ui/js/printmotor/sizeme-printmotor.js
@@ -1,7 +1,7 @@
 var sizeme_UI_options = {
 	appendContentTo: ".sizeme-size-guide",
 	appendSplashTo: ".descriptionholder .variationdataholder-size",
-	sizeSelectionContainer: ".variationdataholder-size",
+	sizeSelectionContainer: ".variationdata-size",
 	addToCartElement: ".addtocartlink",
 	addToCartEvent: "click",
 	lang: "en"
