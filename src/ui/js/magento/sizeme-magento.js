bd646eaf8520d4096874cfb5e28e4217433eb9fa
Ville Könönen <ville@nomasi.com>
2016-12-15 16:00:34 +0200

    Added invokeElement and invokeEvent to invoke SizeMe updates
    This is to improve functionality with two (or more) parameters such as size and color

--- src/ui/js/magento/sizeme-magento.js
+++ src/ui/js/magento/sizeme-magento.js
@@ -3,8 +3,9 @@ SizeMe.UI.options = {
     appendContentTo: ".product-options",
     appendSplashTo: ".product-options",
     sizeSelectionElement: "select.sizeme-magento-size-selector",
+	invokeElement: "select.super-attribute-select",
+	invokeEvent: "change",
 	addToCartElement: "form#product_addtocart_form a.liftup-button, form#product_addtocart_form button",
 	addToCartEvent: "click",
-    firstRecommendation: true,
-	sizeChangeClickElement: ""
+    firstRecommendation: true
 };
